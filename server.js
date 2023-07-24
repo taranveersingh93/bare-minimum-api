840d474da8f7e5fdb167815401a0e1ea80b27dd4
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-24 16:49:54 -0600

    feat: add dynamic route skeleton

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.get('/api/v1/tasks', (req, res) => {
   res.status(200).json({ tasks: app.locals.tasks });
 });
 
+app.get('/api/v1/tasks/:category', (req, res) => {
+  let categoryKeys = Object.keys(app.locals.tasks)
+  const { category } = req.params;
+  const specificCategory = categoryKeys.find(task => task.category === category)
+  res.status(200).json(findCategory)
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
