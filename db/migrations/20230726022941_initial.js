9775636200a6b936001c82c9062459b21ca9a202
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-25 22:50:38 -0400

    Write the migration code

--- db/migrations/20230726022941_initial.js
+++ db/migrations/20230726022941_initial.js
@@ -3,7 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('allTasks', function(table) {
+    table.increments('id').primary();
+    table.string('category');
+    table.string('task');
+    table.string('seen');
+    table.string('saved');
+    table.string('completed');
+    table.timestamps();
+  })
 };
 
 /**
@@ -11,5 +19,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('allTasks')
 };
