b2cf4bde1b4c4d5a6b985c20f6d829c9712c97be
Taranveer Singh <taranveersingh93@gmail.com>
2023-07-27 20:52:03 -0400

    Hardcode environment to prod

--- knex.js
+++ knex.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('./knexfile');
 const environment = process.env.NODE_ENV || 'development';
-const knexConfig = config[environment];
+const knexConfig = config['production'];
 
 module.exports = knex(knexConfig);
